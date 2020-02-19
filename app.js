f90c59088168fd6ee1e0f53ce946ad5ee8c2062b
Hammed Ishola <hammedM@Hammed-Notebook.local>
2020-02-19 16:52:48 -0600

    app.js update for heroku mongoose.connrct.. link

--- app.js
+++ app.js
@@ -2,23 +2,26 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 const _ = require("lodash");
-mongoose.connect('mongodb://localhost/todolistDB', {useNewUrlParser: true ,  useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-hammed:Testing123@cluster0-a0tew.mongodb.net/todolistDB" , {useNewUrlParser: true, useUnifiedTopology: true  }); // connect to mongodb server
 mongoose.set('useFindAndModify', false);
 const app = express();
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
-const itemsSchema = new mongoose.Schema({                // data prototype
+const itemsSchema = new mongoose.Schema({ // data prototype
   name: String
 });
 
-const Item = mongoose.model("Item", itemsSchema);       // follow the prototype. Note: Item is the model so whenever we want to
-const item1 = new Item({                                // make any changes to the database, we have to make reference to ex List.change()
+const Item = mongoose.model("Item", itemsSchema); // follow the prototype. Note: Item is the model so whenever we want to
+const item1 = new Item({ // make any changes to the database, we have to make reference to ex List.change()
   name: "Welcome to your todolist"
 });
 
@@ -29,50 +32,55 @@ const item2 = new Item({
 const item3 = new Item({
   name: "<-- Hit this button to delete an item"
 });
-const defaultItems = [item1 , item2, item3];
+const defaultItems = [item1, item2, item3];
 
-const listSchema = {                                   // schema of our object version for our dynamic route parameter
+const listSchema = { // schema of our object version for our dynamic route parameter
   name: String,
   items: [itemsSchema]
 };
-const List = mongoose.model("List" , listSchema);       // for our dynamic route parameter
+const List = mongoose.model("List", listSchema); // for our dynamic route parameter
 
-app.get("/", function(req, res) {          // the favicon.ico that apears in the database name seems to be springing from get request domain
+app.get("/", function(req, res) { // the favicon.ico that apears in the database name seems to be springing from get request domain
 
-  Item.find({  }, function(err, foundItem){
+  Item.find({}, function(err, foundItem) {
 
-      if(foundItem.length === 0){                        // checking if the document is empty
+    if (foundItem.length === 0) { // checking if the document is empty
 
-      Item.insertMany(defaultItems , function(err){      // The default item in this case can be the user items
-        if(err){
-        console.log(err);
-        }else{
+      Item.insertMany(defaultItems, function(err) { // The default item in this case can be the user items
+        if (err) {
+          console.log(err);
+        } else {
 
-        console.log("Successfully saved the default items");
+          console.log("Successfully saved the default items");
         }
       });
       res.redirect("/");
-    }else{
-      res.render("list", {listTitle: "Today", newListItems: foundItem});        //foundItem is an Array type due to defaultItems being an Array
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItem
+      }); //foundItem is an Array type due to defaultItems being an Array
     }
 
   });
 
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   const itemName = req.body.newItem;
   const listName = req.body.list;
 
   const item = new Item({
-  name: itemName
+    name: itemName
   });
 
-  if(listName === "Today"){
+  if (listName === "Today") {
     item.save();
     res.redirect("/");
-  }else{
-    List.findOne({name: listName} , function(err , foundList){
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
       foundList.items.push(item);
       foundList.save();
       res.redirect("/" + listName);
@@ -81,58 +89,73 @@ app.post("/", function(req, res){
 
 });
 
-app.post("/delete", function(req , res){
+app.post("/delete", function(req, res) {
   const checkedItemId = req.body.deleteItem;
   const listName = req.body.listName;
 
-  if(listName === "Today"){
-    Item.findByIdAndRemove({_id: checkedItemId} , function(err){
-    if(!err){
-    console.log("Sucessfully deleted checked item");
-    }
+  if (listName === "Today") {
+    Item.findByIdAndRemove({
+      _id: checkedItemId
+    }, function(err) {
+      if (!err) {
+        console.log("Sucessfully deleted checked item");
+      }
     });
     res.redirect("/");
-  }else{
-    List.findOneAndUpdate({name: listName}, {$pull: {items:{_id: checkedItemId}}}, function(err , found){
-    if(!err){
-      res.redirect("/" + listName);
-    }
+  } else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull: {
+        items: {
+          _id: checkedItemId
+        }
+      }
+    }, function(err, found) {
+      if (!err) {
+        res.redirect("/" + listName);
+      }
     });
   }
 
   //res.redirect("/");
 });
 
-app.get("/:pageName", function (req, res) {
-  const pageEntry = _.capitalize(req.params.pageName);     // using lodash to capitalize the the first letter of pageEntry
-  List.findOne({name: pageEntry} , function(err , foundList){   // it finds one data in particular that meet the condition
-                                                                          // of the first argument and stores in callback 2nd param
-    if(!err){
-      if(!foundList){                       // foundList which is 2nd argument compare user's pageName entry with the one in the database
+app.get("/:pageName", function(req, res) {
+  const pageEntry = _.capitalize(req.params.pageName); // using lodash to capitalize the the first letter of pageEntry
+  List.findOne({
+    name: pageEntry
+  }, function(err, foundList) { // it finds one data in particular that meet the condition
+    // of the first argument and stores in callback 2nd param
+    if (!err) {
+      if (!foundList) { // foundList which is 2nd argument compare user's pageName entry with the one in the database
         //console.log(foundList.name);
         const list = new List({
-          name: pageEntry,                                     //req.params.pageName,
+          name: pageEntry, //req.params.pageName,
           items: defaultItems
         });
-      //  List.deleteMany({} , function(err , foundList){   });
-      list.save();
-      res.redirect("/" + pageEntry);
-      }else{
+        //  List.deleteMany({} , function(err , foundList){   });
+        list.save();
+        res.redirect("/" + pageEntry);
+      } else {
 
         //console.log("Does exist");
-        res.render("list" , {listTitle: foundList.name, newListItems: foundList.items});
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        });
       }
     }
-   });
+  });
 
 });
 
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
-let port = process.env.PORT;                // to access it both locally and remotely with if condition below
+let port = process.env.PORT; // to access it both locally and remotely with if condition below
 if (port == null || port == "") {
   port = 3000;
 }
